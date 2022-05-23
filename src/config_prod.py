e044252b6e6dead06a4e3f7fd277ff040f32a552
greinisch <greinisch@youwol.com>
2022-05-23 11:20:51 +0200

    👽️ DeployedContextLogger renamed DeployedContextReporter

--- src/config_prod.py
+++ src/config_prod.py
@@ -6,7 +6,7 @@ from youwol_flux_backend import Configuration, Constants
 
 from youwol_utils import StorageClient, DocDbClient, AuthClient, CacheClient, get_headers_auth_admin_from_env
 from youwol_utils.clients.assets_gateway.assets_gateway import AssetsGatewayClient
-from youwol_utils.context import DeployedContextLogger
+from youwol_utils.context import DeployedContextReporter
 from youwol_utils.http_clients.flux_backend import PROJECTS_TABLE, COMPONENTS_TABLE
 from youwol_utils.middlewares import Middleware
 from youwol_utils.servers.fast_api import FastApiMiddleware, AppConfiguration, ServerOptions
@@ -58,7 +58,7 @@ async def get_configuration():
             )
         ],
         on_before_startup=_on_before_startup,
-        ctx_logger=DeployedContextLogger()
+        ctx_logger=DeployedContextReporter()
     )
     return AppConfiguration(
         server=server_options,
